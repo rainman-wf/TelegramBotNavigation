9b0337835f8b13a8d2aa36724487667d3d87c734
rainman-wf <denviva@gmail.com>
2023-03-27 09:21:34 +0300

    try jv 17 in github actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("multiplatform") version "1.7.0"
+    kotlin("jvm") version "1.8.10"
     id("maven-publish")
 }
 
@@ -11,21 +11,13 @@ repositories {
 }
 
 dependencies {
-    commonMainImplementation("com.github.pengrad:java-telegram-bot-api:6.5.0")
-    commonMainImplementation("com.squareup.retrofit2:retrofit:2.7.2")
-    commonMainImplementation("com.squareup.retrofit2:converter-gson:2.7.2")
-    commonMainImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.6.4")
-    commonMainImplementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+    implementation("com.github.pengrad:java-telegram-bot-api:6.5.0")
+    implementation("com.squareup.retrofit2:retrofit:2.7.2")
+    implementation("com.squareup.retrofit2:converter-gson:2.7.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.6.4")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
 }
 
-kotlin {
-    jvm {
-        compilations.all {
-            kotlinOptions.jvmTarget = "11"
-        }
-        withJava()
-    }
-}
 
 publishing {
     publications {
