3734e7c00e10e9a0366dcf79cf45721ca77bde06
rainman-wf <denviva@gmail.com>
2023-10-13 21:36:47 +0300

    who knows, compile failed

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,9 +21,10 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.6.4")
     implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.10")
+    implementation(kotlin("stdlib-jdk8"))
     implementation("com.google.dagger:dagger:2.48")
 }
+
 
 publishing {
     publications {
@@ -36,6 +37,8 @@ publishing {
     }
 }
 
+
+
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(11)
 }
